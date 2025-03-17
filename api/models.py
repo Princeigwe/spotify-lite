58076b27274b6412f84911f99a4f9f6a65828391
Prince Igwe <igwep297@gmail.com>
2025-03-17 13:46:45 +0100

    nodes and relationships created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+import neomodel
+import datetime
+import pytz
 
-# Create your models here.
+
+class Genre(neomodel.StructuredNode):
+    name = neomodel.StringProperty(unique_index=True, required=True)
+    
+    track = neomodel.RelationshipTo('Track', 'HAS_TRACK')
+    
+
+class Artist(neomodel.StructuredNode):
+    name = neomodel.StringProperty(unique_index=True, required=True)
+    age = neomodel.IntegerProperty()
+    is_producer = neomodel.BooleanProperty(default=False)
+    place = neomodel.StringProperty()
+    website = neomodel.StringProperty()
+    followers = neomodel.IntegerProperty()
+
+    produced = neomodel.RelationshipTo('Album', 'PRODUCED')
+    collaboed_with = neomodel.RelationshipTo('Artist', 'COLLABOED_WITH')
+    sang_in = neomodel.RelationshipTo('Track', 'SANG_IN')
+
+
+class Album(neomodel.StructuredNode):
+    name = neomodel.StringProperty(unique_index=True, required=True)
+    release_date = neomodel.DateTimeProperty(default=lambda: datetime.datetime.now(pytz.utc), index=True)
+    cover_art = neomodel.StringProperty()
+
+    has_track = neomodel.RelationshipTo('Track', 'HAS_TRACK')
+
+
+class Track(neomodel.StructuredNode):
+    title = neomodel.StringProperty(unique_index=True, required=True)
+    length = neomodel.IntegerProperty()
+
+
+
+class Customer(neomodel.StructuredNode):
+    name = neomodel.StringProperty(unique_index=True, required=True)
+    age = neomodel.IntegerProperty()
+    email = neomodel.StringProperty()
+
+    liked = neomodel.RelationshipTo('Track', 'LIKED')
+
